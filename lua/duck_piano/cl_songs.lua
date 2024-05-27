f751c48a2d744e56f397d58b4343da2da4cbf360
DeadDuck <46423255+Dead0Duck@users.noreply.github.com>
2024-05-28 00:05:36 +0700

    ✨ Добавлять песни только из папки songs

--- lua/duck_piano/cl_songs.lua
+++ lua/duck_piano/cl_songs.lua
@@ -2,13 +2,16 @@ duckInstruments = {}
 duckInstruments.songs = {}
 duckInstruments.songNames = {}
 
-function duckInstruments.AddSong(n, v)
+local function AddSong(n, v)
 	local i = #duckInstruments.songs + 1
 	duckInstruments.songs[i] = v
 	duckInstruments.songNames[i] = n
 end
 
+-- Разрешить добавление песен только из songs/*
 local songFiles = file.Find( 'duck_piano/songs/*', 'LUA' )
+duckInstruments.AddSong = AddSong
 for _,fileName in pairs(songFiles) do
 	include('duck_piano/songs/' .. fileName)
 end
+duckInstruments.AddSong = nil
